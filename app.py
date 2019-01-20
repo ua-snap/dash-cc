162f59e5bb3df9558b620cefdc24e127f5cdc574
Alec Bennett <alec@alecbennett.com>
2019-01-19 15:05:57 -0900

    Adding more scenarios worth of data, plots for scenarios, buttons, etc.

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ server.secret_key = os.environ.get('secret_key', 'secret')
 
 #df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/hello-world-stock.csv')
 df = pd.read_csv('communities.csv')
-variable = 'temperature'
 units = 'standard'
 rcp = '6.5'
 baseline = 'cru'
@@ -29,7 +28,7 @@ dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.j
 app.layout = html.Div([
     html.H1('Community Charts'),
     dcc.Dropdown(
-        id='my-dropdown',
+        id='community',
         options=[
             {'label': 'Anchorage', 'value': 'Anchorage'},
             {'label': 'Fairbanks', 'value': 'Fairbanks'},
@@ -37,108 +36,235 @@ app.layout = html.Div([
         ],
         value='Anchorage'
     ),
+    html.Div(dcc.Markdown('xyz',id='div1')),
     html.Button('Temperature', id='temperature'),
     html.Button('Precipitation', id='precipitation'),
-    dcc.Graph(id='my-graph')
+    html.Div(dcc.Markdown('abcd',id='div2')),
+    html.Button('RCP45', id='rcp45'),
+    html.Button('RCP60', id='rcp60'),
+    html.Button('RCP85', id='rcp85'),
+    dcc.Graph(id='ccharts')
 ], className="container")
 
-@app.callback(Output('my-graph', 'figure'),
-              [Input('my-dropdown', 'value')])
+@app.callback(Output('div1', 'children'),
+    inputs=[
+      Input('temperature', 'n_clicks_timestamp'),
+      Input('precipitation', 'n_clicks_timestamp')])
+def update_div1(temp, precip):
+    if (precip > temp):
+        return "Precip CLICKED"
+    else:
+        return "Temp CLICKED"
+@app.callback(Output('div2', 'children'),
+    inputs=[
+      Input('rcp45', 'n_clicks_timestamp'),
+      Input('rcp60', 'n_clicks_timestamp'),
+      Input('rcp85', 'n_clicks_timestamp')])
+def update_div2(rcp45, rcp60, rcp85):
+    if (rcp45 > rcp60 and rcp45 > rcp85):
+        return "RCP45 CLICKED"
+    elif (rcp60 > rcp45 and rcp60 > rcp85):
+        return "RCP60 CLICKED"
+    else:
+        return "RCP85 CLICKED"
 
-def update_graph(selected_dropdown_value):
+@app.callback(Output('ccharts', 'figure'),
+    inputs=[
+      Input('community', 'value'), 
+      Input('temperature', 'n_clicks_timestamp'), 
+      Input('precipitation', 'n_clicks_timestamp'),
+      Input('rcp45', 'n_clicks_timestamp'),
+      Input('rcp60', 'n_clicks_timestamp'),
+      Input('rcp85', 'n_clicks_timestamp')
+    ])
+def update_graph(selected_dropdown_value, temp, precip, rcp45, rcp60, rcp85):
     dff = df[df['Community'] == selected_dropdown_value]
+    df0 = dff;
+    if (rcp45 > rcp60 and rcp45 > rcp85):
+        df0 = df[dff['Scenario'] == '4.5']
+    elif (rcp60 > rcp45 and rcp60 > rcp85):
+        df0 = df[dff['Scenario'] == '6.0']
+    else:
+        df0 = df[dff['Scenario'] == '8.5']
     dfhist = df[dff['Decades'] == '1961-1990']
-    df10s = df[dff['Decades'] == '2010-2019']
-    df40s = df[dff['Decades'] == '2090-2099']
-    df60s = df[dff['Decades'] == '2060-2069']
-    df90s = df[dff['Decades'] == '2090-2099']
-    return {
-        'data': [{
-            'x': dfhist.Month,
-            'y': dfhist.Temp - 32,
-            'type': 'bar',
-            'base': 32,
-            'marker': {
-                'color': '#999999'
-            },
-            'name': 'Historical'
-        },{
-            'x': df10s.Month,
-            'y': df10s.Temp - 32,
-            'type': 'bar',
-            'base': 32,
-            'marker': {
-                'color': '#fecc5c'
-            },
-            'name': '2010-2019',
-            'error_y': {
-                'type': 'data',
-                'array': df10s.Error,
-                'visible': 'true'
+    df10s = df0[dff['Decades'] == '2010-2019']
+    df40s = df0[dff['Decades'] == '2090-2099']
+    df60s = df0[dff['Decades'] == '2060-2069']
+    df90s = df0[dff['Decades'] == '2090-2099']
+    if (temp > precip or temp == precip):
+        return {
+            'data': [{
+                'x': dfhist.Month,
+                'y': dfhist.Temp - 32,
+                'type': 'bar',
+                'base': 32,
+                'marker': {
+                    'color': '#999999'
+                },
+                'name': 'Historical'
+            },{
+                'x': df10s.Month,
+                'y': df10s.Temp - 32,
+                'type': 'bar',
+                'base': 32,
+                'marker': {
+                    'color': '#bae4bc'
+                },
+                'name': '2010-2019',
+                'error_y': {
+                    'type': 'data',
+                    'array': df10s.TError,
+                    'visible': 'true'
+                }
+            },{
+                'x': df40s.Month,
+                'y': df40s.Temp - 32,
+                'type': 'bar',
+                'base': 32,
+                'marker': {
+                    'color': '#7bccc4'
+                },
+                'name': '2040-2049',
+                'error_y': {
+                    'type': 'data',
+                    'array': df40s.TError,
+                    'visible': 'true'
+                }
+            },{
+                'x': df60s.Month,
+                'y': df60s.Temp - 32,
+                'type': 'bar',
+                'base': 32,
+                'marker': {
+                    'color': '#43a2ca'
+                },
+                'name': '2060-2069',
+                'error_y': {
+                    'type': 'data',
+                    'array': df60s.TError,
+                    'visible': 'true'
+                }
+            },{
+                'x': df90s.Month,
+                'y': df90s.Temp - 32,
+                'type': 'bar',
+                'base': 32,
+                'marker': {
+                    'color': '#0868ac'
+                },
+                'name': '2090-2099',
+                'error_y': {
+                    'type': 'data',
+                    'array': df90s.TError,
+                    'visible': 'true'
+                }
+            }],
+            'layout': {
+                'barmode': 'grouped',
+                'yaxis': {
+                    'zeroline': 'false',
+                    'zerolinecolor': '#efefef',
+                    'zerolinewidth': 0.5
+                },
+                'margin': {
+                    'l': 30,
+                    'r': 20,
+                    'b': 30,
+                    't': 20
+                },
+                'shapes': [{
+                    'type': 'line', 
+                    'x0': 0, 'x1': 1, 'xref': 'paper',
+                    'y0': 32, 'y1': 32, 'yref': 'y',
+                    'line': { 'width': 1 }
+                }]
             }
-        },{
-            'x': df40s.Month,
-            'y': df40s.Temp - 32,
-            'type': 'bar',
-            'base': 32,
-            'marker': {
-                'color': '#fd8d3c'
-            },
-            'name': '2040-2049',
-            'error_y': {
-                'type': 'data',
-                'array': df40s.Error,
-                'visible': 'true'
-            }
-        },{
-            'x': df60s.Month,
-            'y': df60s.Temp - 32,
-            'type': 'bar',
-            'base': 32,
-            'marker': {
-                'color': '#f03b20'
-            },
-            'name': '2060-2069',
-            'error_y': {
-                'type': 'data',
-                'array': df60s.Error,
-                'visible': 'true'
-            }
-        },{
-            'x': df90s.Month,
-            'y': df90s.Temp - 32,
-            'type': 'bar',
-            'base': 32,
-            'marker': {
-                'color': '#bd0026'
-            },
-            'name': '2090-2099',
-            'error_y': {
-                'type': 'data',
-                'array': df90s.Error,
-                'visible': 'true'
-            }
-        }],
-        'layout': {
-            'barmode': 'grouped',
-            'zeroline': 'false',
-            'yaxis': {
-                'zeroline': 'false',
-            },
-            'margin': {
-                'l': 30,
-                'r': 20,
-                'b': 30,
-                't': 20
-            },
-            'shapes': [{
-                'type': 'line', 
-                'x0': 0, 'x1': 1, 'xref': 'paper',
-                'y0': 32, 'y1': 32, 'yref': 'y',
-                'line': { 'width': 1 }
-            }]
         }
-    }
+    else:
+        return {
+            'data': [{
+                'x': dfhist.Month,
+                'y': dfhist.Precip,
+                'type': 'bar',
+                'marker': {
+                    'color': '#999999'
+                },
+                'name': 'Historical'
+            },{
+                'x': df10s.Month,
+                'y': df10s.Precip,
+                'type': 'bar',
+                'marker': {
+                    'color': '#fecc5c'
+                },
+                'name': '2010-2019',
+                'error_y': {
+                    'type': 'data',
+                    'array': df10s.PError,
+                    'visible': 'true'
+                }
+            },{
+                'x': df40s.Month,
+                'y': df40s.Precip,
+                'type': 'bar',
+                'marker': {
+                    'color': '#fd8d3c'
+                },
+                'name': '2040-2049',
+                'error_y': {
+                    'type': 'data',
+                    'array': df40s.PError,
+                    'visible': 'true'
+                }
+            },{
+                'x': df60s.Month,
+                'y': df60s.Precip,
+                'type': 'bar',
+                'marker': {
+                    'color': '#f03b20'
+                },
+                'name': '2060-2069',
+                'error_y': {
+                    'type': 'data',
+                    'array': df60s.PError,
+                    'visible': 'true'
+                }
+            },{
+                'x': df90s.Month,
+                'y': df90s.Precip,
+                'type': 'bar',
+                'marker': {
+                    'color': '#bd0026'
+                },
+                'name': '2090-2099',
+                'error_y': {
+                    'type': 'data',
+                    'array': df90s.PError,
+                    'visible': 'true'
+                }
+            }],
+            'layout': {
+                'barmode': 'grouped',
+                'yaxis': {
+                    'zeroline': 'false',
+                    'zerolinecolor': '#efefef',
+                    'zerolinewidth': 0.5
+                },
+                'margin': {
+                    'l': 30,
+                    'r': 20,
+                    'b': 30,
+                    't': 20
+                },
+                'shapes': [{
+                    'type': 'line', 
+                    'x0': 0, 'x1': 1, 'xref': 'paper',
+                    'y0': 32, 'y1': 32, 'yref': 'y',
+                    'line': { 'width': 1 }
+                }]
+            }
+        }
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run_server(debug=True, host='0.0.0.0', port=port)
